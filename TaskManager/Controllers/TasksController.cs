c6987a0de5ec66df2df8731f4556afd792214ec8
Lorenzo <aslollo99@gmail.com>
2025-08-20 12:21:53 +0200

    first commit

---
+++ TaskManager/Controllers/TasksController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManager.Data;
+using TaskManager.Models;
+
+namespace TaskManager.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TasksController : ControllerBase
+{
+    private static readonly List<TaskItem> Tasks = new();
+    private readonly AppDbContext _context;
+    
+    public TasksController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        // Ritorno tutti i task
+        var tasks = _context.Tasks.ToList();
+        return Ok(tasks);
+    }
+
+    [HttpPost]
+    public IActionResult Add([FromBody] TaskItem  task)
+    {
+        _context.Tasks.Add(task);
+        _context.SaveChanges();
+        return Ok(new { message = "Task aggiunto", value = task });
+    }
+
+}
---
+++ TaskManager/Models/TaskItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManager.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }   // chiave primaria
+    public string Title { get; set; } = string.Empty;
+    public bool IsDone { get; set; } = false;
+}
